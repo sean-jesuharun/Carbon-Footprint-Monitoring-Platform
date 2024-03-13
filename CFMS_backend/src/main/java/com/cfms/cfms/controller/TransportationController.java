3f5458f874de0217fb04ae1b51a01570eb79552d
sean-jesuharun <80287302+sean-jesuharun@users.noreply.github.com>
2024-03-13 20:51:02 +0530

    Updating Inventory Table Product Quantity According to INBOUND & OUTBOUND Transportation

--- CFMS_backend/src/main/java/com/cfms/cfms/controller/TransportationController.java
+++ CFMS_backend/src/main/java/com/cfms/cfms/controller/TransportationController.java
@@ -40,40 +40,41 @@ public class TransportationController {
     }
 
     @PostMapping(value = {"/transportationData/{vehicle_id}/{transport_inventory}", "/transportationData/{vehicle_id}/{transport_inventory}/{vendor_id}"})
-    public ResponseEntity<Integer> transportationDataProcess(@PathVariable("vehicle_id") Long vehicle_id, @PathVariable("transport_inventory") String transportInventory, @PathVariable(value = "vendor_id", required = false) Long vendor_id, @RequestBody Transportation transportation){
+    public ResponseEntity<String> transportationDataProcess(@PathVariable("vehicle_id") Long vehicle_id, @PathVariable("transport_inventory") String transportInventory, @PathVariable(value = "vendor_id", required = false) Long vendor_id, @RequestBody Transportation transportation){
 
         // Setting vehicle for the Transportation.
         transportation.setVehicle(vehicleService.getVehicle(vehicle_id));
+
+
+//        // Create a WebClient instance using the WebClientBuilder.
+//        // Deployed flask API endpoint url.
+//        WebClient webClient = webClientBuilder.baseUrl("https://cfms-model.el.r.appspot.com").build();
+//
+//        // Local flask API server url.
+//        //WebClient webClient = webClientBuilder.baseUrl("http://127.0.0.1:5000").build();
+//
+//        // Send a POST request to the "/transportationEmissionPrediction" endpoint.
+//        // Set the content type of the request body to JSON.
+//        // Serialize the 'transportation' object and include it in the request body.
+//        // Retrieve the response from the server.
+//        // Deserializing the JSON response into a Map<String, Integer> using ParameterizedTypeReference
+//        // Mapping the response to extract the 'co2Emission' value from the map
+//        // Blocking the execution until the response is received.
+//        Integer predictedTransportationCo2Emission = webClient.post()
+//                .uri("/transportationEmissionPrediction")
+//                .contentType(MediaType.APPLICATION_JSON)
+//                .body(BodyInserters.fromValue(new TransportationEmissionData(transportation.getVehicle().getModel().getModel(),transportation.getVehicle().getEngine_size(), transportation.getVehicle().getCylinders(), transportation.getFuel_consumption(), transportation.getVehicle().getVehicle_class(), transportation.getFuel_type())))
+//                .retrieve()
+//                .bodyToMono(new ParameterizedTypeReference<Map<String, Integer>>() {})
+//                .map(response -> response.get("co2Emission"))
+//                .block();
+
 
         // Checking whether the Transportation is INBOUND or OUTBOUND
         if (vendor_id != null) {
             // Setting vendor for the Transportation.
             transportation.setVendor(vendorService.getVendor(vendor_id));
         }
-
-        // Create a WebClient instance using the WebClientBuilder.
-        // Deployed flask API endpoint url.
-        WebClient webClient = webClientBuilder.baseUrl("https://cfms-model.el.r.appspot.com").build();
-
-        // Local flask API server url.
-        //WebClient webClient = webClientBuilder.baseUrl("http://127.0.0.1:5000").build();
-
-        // Send a POST request to the "/transportationEmissionPrediction" endpoint.
-        // Set the content type of the request body to JSON.
-        // Serialize the 'transportation' object and include it in the request body.
-        // Retrieve the response from the server.
-        // Deserializing the JSON response into a Map<String, Integer> using ParameterizedTypeReference
-        // Mapping the response to extract the 'co2Emission' value from the map
-        // Blocking the execution until the response is received.
-        Integer predictedTransportationCo2Emission = webClient.post()
-                .uri("/transportationEmissionPrediction")
-                .contentType(MediaType.APPLICATION_JSON)
-                .body(BodyInserters.fromValue(new TransportationEmissionData(transportation.getVehicle().getModel().getModel(),transportation.getVehicle().getEngine_size(), transportation.getVehicle().getCylinders(), transportation.getFuel_consumption(), transportation.getVehicle().getVehicle_class(), transportation.getFuel_type())))
-                .retrieve()
-                .bodyToMono(new ParameterizedTypeReference<Map<String, Integer>>() {})
-                .map(response -> response.get("co2Emission"))
-                .block();
-
 
         // Saving Transportation
         transportation = transportationService.saveTransportation(transportation);
@@ -87,14 +88,21 @@ public class TransportationController {
             // Saving TransportationInventory
             transportationInventoryService.saveTransportationInventory(new TransportationInventory(new TransportationInventoryKey(transportation, inventoryService.getInventory(productId)), transportInventoriesQuantityList.get(productId)));
 
+            // Updating Inventory
+            if (transportation.getTransportation_type().equals("INBOUND")){
+                inventoryService.addInventory(productId, transportInventoriesQuantityList.get(productId));
+            } else {
+                inventoryService.removeInventory(productId, transportInventoriesQuantityList.get(productId));
+            }
+
         }
 
         // Return the response received from Flask endpoint to the client
-        return ResponseEntity.ok(predictedTransportationCo2Emission);
+        return ResponseEntity.ok("Success");
 
     }
 
-    // Converting the String of transport_inventory to Map <Long, Integer> inventory_id, Quantity
+    // Converting the String of transport_inventory to Map <Long, Integer> <inventory_id, Quantity>
     private Map<Long, Integer> parseTransportInventoryString(String propertyIds) {
         Map<Long, Integer> result = new HashMap<>();
         String[] pairs = propertyIds.split(",");
--- CFMS_backend/src/main/java/com/cfms/cfms/service/InventoryService.java
+++ CFMS_backend/src/main/java/com/cfms/cfms/service/InventoryService.java
@@ -18,4 +18,16 @@ public class InventoryService {
     public Inventory getInventory(Long id){
         return inventoryRepository.getReferenceById(id);
     }
+
+    public void addInventory(Long product_id, int quantity){
+        Inventory inventory = inventoryRepository.getReferenceById(product_id);
+        inventory.setQuantity_on_hand(inventory.getQuantity_on_hand() + quantity);
+        inventoryRepository.save(inventory);
+    }
+
+    public void removeInventory(Long product_id, int quantity){
+        Inventory inventory = inventoryRepository.getReferenceById(product_id);
+        inventory.setQuantity_on_hand(inventory.getQuantity_on_hand() - quantity);
+        inventoryRepository.save(inventory);
+    }
 }
