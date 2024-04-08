f737fdc3f27443d32403b149e37a4c009d8ff739
sean-jesuharun <80287302+sean-jesuharun@users.noreply.github.com>
2024-04-08 23:04:42 +0530

    Implemented API-Gateway Microservice

---
+++ CFMS_Backend/CFMS_Microservices/API-Gateway-CFMS/src/main/java/com/cfms/apigatewaycfms/ApiGatewayCfmsApplication.java
@@ -0,0 +1,13 @@
+package com.cfms.apigatewaycfms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiGatewayCfmsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ApiGatewayCfmsApplication.class, args);
+    }
+
+}
