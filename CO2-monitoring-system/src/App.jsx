f5b9b33142ccaf4d479e9e9e47f083837e0ba627
Channa Dissanayaka <ywchanna@gmail.com>
2024-05-14 14:52:21 +0530

    Created CarbonEmissionEvaluationForm

--- CO2-monitoring-system/src/App.jsx
+++ CO2-monitoring-system/src/App.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import MLInputForm from './MLInputForm'; // Assuming MLInputForm is in the same directory
 import VenderInputFrom from './VenderInputForm';
+import CarbonEmissionEvaluationFrom from './CorbonEmissionEvaluationForm'
 import { green } from '@mui/material/colors';
 import {teal} from '@mui/material/colors';
 import { Typography } from '@mui/material';
@@ -9,8 +10,9 @@ import { Typography } from '@mui/material';
 function App() {
   return (
     <div className="App" style={{ backgroundColor: '#eceff1' }} >     
-      {/* <MLInputForm /> */}
+      <MLInputForm />
       <VenderInputFrom/>
+      <CarbonEmissionEvaluationFrom/>
     </div>
   );
 }
